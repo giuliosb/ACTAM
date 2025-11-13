f8306a6555c2440b7876c803e74b8055a1d8153f
giuliosb <giuliosbe@gmail.com>
2025-11-13 16:00:07 +0100

    update keyboard

--- frontend/src/components/Keyboard.jsx
+++ frontend/src/components/Keyboard.jsx
@@ -6,23 +6,23 @@ const NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 function Keyboard() {
   const [a4Frequency, setA4Frequency] = useState(440);
 
-  // calcola la frequenza di una nota in temperamento equabile
+  // frequency calculation
   const noteFrequency = (note, octave) => {
-    const n = NOTES.indexOf(note) + (octave - 4) * 12 - 9; // distanza in semitoni da A4
+    const n = NOTES.indexOf(note) + (octave - 4) * 12 - 9; // semitone distance from A4
     return a4Frequency * Math.pow(2, n / 12);
   };
 
-  // genera tutte le note tra C3 e C5
+  // notes generation
   const keys = [];
   const octaves = [3, 4, 5];
   for (let o of octaves) {
     for (let note of NOTES) {
-      // fermati dopo C5
+
       if (o === 5 && note === "C") {
         keys.push({ note, octave: o, freq: noteFrequency(note, o) });
         break;
       }
-      // aggiungi tutte le altre note
+      
       if (!(o === 5 && note !== "C")) {
         keys.push({ note, octave: o, freq: noteFrequency(note, o) });
       }
